b81f7459077bf335ff7b60646813e780bca726ec
MEDFAIEZ17 <147945387+MEDFAIEZ17@users.noreply.github.com>
2024-09-17 19:37:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/sup.css">
+</head>
+<header>
+    <div class="logo">
+        <img src="img/pvrai.png" alt="Logo de l'entreprise">
+    </div>
+    <div class="title">
+        <h1>Espace de gestion de Roussource Humaine SEBN-TN</h1>
+    </div>  
+<!-- <nav>
+        <ul>
+            <li><a href="page_personnelle.php">Page personnelle</a></li>
+            <li><a href="index.php">Stuff</a></li>
+            <li><a href="dashboard.php">Tableaux de Bord</a></li>
+            <li><a href="learn_more.php">Learn more</a></li>
+        </ul>
+</nav>-->
+    <div id="dec">
+        <a href="View/login.php" id="dectxt">Se connecter</a>
+    </div>
+</header>
+    <style>
+        h1 {
+            font-size: 2.5rem;
+            color: #333;
+        }
+        p {
+            font-size: 1.2rem;
+            color: #555;
+            text-align: center;
+            max-width: 600px;
+
+        }
+        .bienvenue {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 80%; 
+            max-width: 600px; 
+            padding: 20px;
+            /*background-color: #f9f9f9;*/
+            background-color: #ADB8DD;
+            border-radius: 10px; 
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
+            text-align: center; 
+        }
+
+        .bienvenue h1 {
+            margin-bottom: 15px;
+            font-size: 2.5em;
+        color: #333;
+        }
+
+        .bienvenue p {
+            font-size: 1.2em;
+            color: #666;
+            line-height: 1.5;
+        }
+
+    </style>
+
+<body>
+    <div class="bienvenue">
+    <h1>Bienvenue</h1>
+    <p>
+        Bienvenue sur notre plateforme de gestion des ressources humaines. 
+        Conçue pour vous offrir une gestion fluide et efficace de vos équipes, 
+        cette solution vous permet de suivre les performances, les formations , les réunions et 
+        bien plus encore, le tout dans un environnement convivial et intuitif.
+    </p>
+    </div>
+
+</body>
+</html>
